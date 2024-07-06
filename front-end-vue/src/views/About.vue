bedbea162ec6609283c4a360a597b46c84382fef
yhstsy <2798213571@qq.com>
2024-07-07 00:34:14 +0800

    feat: 前端框架搭建

---
+++ front-end-vue/src/views/About.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        About
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'About' ,
+    components: {},
+}
+</script>
+
+<style scoped>
+    
+</style>
