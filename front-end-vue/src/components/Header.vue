62b9d71659dd7cd7a895759c0d0a23a128026d78
yhstsy <2798213571@qq.com>
2024-07-07 00:34:14 +0800

    feat: 前端框架搭建

---
+++ front-end-vue/src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="header">
+        <img class="logo" src="@/assets/logo.png" alt="">
+        <span class="title">图书管理系统</span>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Header' ,
+}
+</script>
+
+<style lang="less" scoped>
+.header{
+    height: 60px;
+    line-height: 60px;
+    background-color: #fff ;
+    margin-bottom: 2px;
+}
+.logo{
+    width: 40px;
+    position: relative;
+    top: 10px;
+    left: 20px;
+}
+.title{
+    margin-left: 25px;
+    font-size: 24px;
+}
+</style>
