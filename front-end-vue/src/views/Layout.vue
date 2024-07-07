bb299e7c618cc9258601a5af9df1d0bd6ec2eb7a
yhstsy <2798213571@qq.com>
2024-07-07 23:47:17 +0800

    feat: 管理员管理

---
+++ front-end-vue/src/views/Layout.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <!-- 头部区域 -->
+    <Header />
+    <!-- 侧边栏和主体 -->
+    <div class="main">
+      <!-- 侧边栏导航 -->
+      <Aside />
+      <!-- 主体数据 -->
+      <div class="container">
+        <router-view />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from "@/components/Header";
+import Aside from "@/components/Aside";
+export default {
+  name: "Layout",
+  components: { Header, Aside },
+};
+</script>
+
+<style lang="less" scoped>
+.main {
+  display: flex;
+  .container {
+    flex: 1;
+    background-color: #fff;
+    padding: 10px;
+  }
+}
+</style>
---
+++ front-end-vue/src/views/home/Home.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div class="item">用户名：{{ admin.adminname }}</div>
+    <div>手机号：{{ admin.phone }}</div>
+    <div>邮箱：{{ admin.email }}</div>
+  </div>
+</template>
+
+<script>
+import Cookies from "js-cookie";
+import request from "@/utils/request";
+export default {
+  name: "Home",
+  data() {
+    return {
+      admin: Cookies.get("admin") ? JSON.parse(Cookies.get("admin")) : {},
+    };
+  },
+  created() {
+    // request.get("/admin/"+this.admin.id, this.form).then((res) => {
+    //   if (res.code === "200") {
+    //     this.admin=res.data
+    //   } else {
+    //     this.$notify.error(res.msg);
+    //   }
+    // });
+  },
+};
+</script>
+
+<style scoped>
+.item {
+  margin: 10px 0;
+}
+</style>
