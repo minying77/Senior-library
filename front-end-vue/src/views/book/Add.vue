967d2371d76b40dea49436fce908f2734e40dc0e
zyq <1372734842@qq.com>
2024-07-09 17:42:07 +0800

    还书

--- front-end-vue/src/views/book/Add.vue
+++ front-end-vue/src/views/book/Add.vue
@@ -38,8 +38,16 @@
       <el-form-item label="数量" prop="nums">
         <el-input v-model="form.nums" placeholder="请输入数量"></el-input>
       </el-form-item>
+      <br>
       <el-form-item label="封面" prop="cover">
-        <el-input v-model="form.cover" placeholder="请输入封面"></el-input>
+        <el-upload
+            class="avatar-uploader"
+            :action="'http://localhost:9090/api/book/file/upload?token='+this.admin.token"
+            :show-file-list="false"
+            :on-success="handleCoverSuccess">
+          <img v-if="form.cover" :src="form.cover" class="avatar">
+          <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+        </el-upload>
       </el-form-item>
     </el-form>
     <div style="text-align: center; margin-bottom: 30px">
@@ -50,6 +58,7 @@
 
 <script>
 import request from "@/utils/request";
+import Cookies from "js-cookie";
 export default {
   name: "AddBook",
   data() {
@@ -64,7 +73,8 @@ export default {
       }, 1000);
     };
     return {
-      form: { score: 10 },
+      admin: Cookies.get("admin") ? JSON.parse(Cookies.get("admin")) : {},
+      form: { score: 10 ,cover:''},
       categories:[],
       rules: {
         name: [
@@ -89,6 +99,11 @@ export default {
     })
   },
   methods: {
+    handleCoverSuccess(res){
+      if(res.code === '200'){
+        this.$set(this.form,'cover',res.data)
+      }
+    },
     save() {
       this.$refs["ruleForm"].validate((valid) => {
         if (valid) {
@@ -107,5 +122,29 @@ export default {
   components: {},
 };
 </script>
-
+<style>
+.avatar-uploader .el-upload {
+  border: 1px dashed #d9d9d9;
+  border-radius: 6px;
+  cursor: pointer;
+  position: relative;
+  overflow: hidden;
+}
+.avatar-uploader .el-upload:hover {
+  border-color: #409EFF;
+}
+.avatar-uploader-icon {
+  font-size: 28px;
+  color: #8c939d;
+  width: 178px;
+  height: 178px;
+  line-height: 178px;
+  text-align: center;
+}
+.avatar {
+  width: 178px;
+  height: 178px;
+  display: block;
+}
+</style>
 <style scoped></style>
--- front-end-vue/src/views/book/Edit.vue
+++ front-end-vue/src/views/book/Edit.vue
@@ -34,8 +34,16 @@
       <el-form-item label="借书积分" prop="bookNo">
         <el-input-number v-model="form.score" :min="10" :max="30" label="所需积分"></el-input-number>
       </el-form-item>
+      <br>
       <el-form-item label="封面" prop="cover">
-        <el-input v-model="form.cover" placeholder="请输入封面"></el-input>
+        <el-upload
+            class="avatar-uploader"
+            :action="'http://localhost:9090/api/book/file/upload?token='+this.admin.token"
+            :show-file-list="false"
+            :on-success="handleCoverSuccess">
+          <img v-if="form.cover" :src="form.cover" class="avatar">
+          <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+        </el-upload>
       </el-form-item>
     </el-form>
     <div style="text-align: center; margin-bottom: 30px">
@@ -46,10 +54,12 @@
 
 <script>
 import request from "@/utils/request";
+import Cookies from "js-cookie";
 export default {
   name: "EditBook",
   data() {
     return {
+      admin: Cookies.get("admin") ? JSON.parse(Cookies.get("admin")) : {},
       form: { score:10 },
       categories:[],
       rules: {
@@ -80,6 +90,11 @@ export default {
     });
   },
   methods: {
+    handleCoverSuccess(res){
+      if(res.code === '200'){
+        this.$set(this.form,'cover',res.data)
+      }
+    },
     save() {
       request.post("/book/update", this.form).then((res) => {
         if (res.code === "200") {
@@ -94,5 +109,29 @@ export default {
   components: {},
 };
 </script>
-
+<style>
+.avatar-uploader .el-upload {
+  border: 1px dashed #d9d9d9;
+  border-radius: 6px;
+  cursor: pointer;
+  position: relative;
+  overflow: hidden;
+}
+.avatar-uploader .el-upload:hover {
+  border-color: #409EFF;
+}
+.avatar-uploader-icon {
+  font-size: 28px;
+  color: #8c939d;
+  width: 178px;
+  height: 178px;
+  line-height: 178px;
+  text-align: center;
+}
+.avatar {
+  width: 178px;
+  height: 178px;
+  display: block;
+}
+</style>
 <style scoped></style>
